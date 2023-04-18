3edd368599f387187a07bffa813bc9d70a5f1bc9
adnanmulalic <adnan.mulalic3@gmail.com>
2023-04-18 11:33:54 +0200

    added dots for changing pictures,styling and animation not finished

--- javascript.js
+++ javascript.js
@@ -4,6 +4,7 @@ const menuOptions = document.querySelector(".menuOptions");
 const pictureFrame = document.querySelector("#pictureFrame");
 const leftButton = document.querySelector("#leftButton");
 const rightButton = document.querySelector("#rightButton");
+const dots = document.querySelector("#dots");
 const pictureFolder= "img/"
 
 function drop(menu) {
@@ -21,17 +22,21 @@ function pictureLoader() {
         newImage.src = "img/pic" + i + ".png";
         newImage.classList.add("picture");
         newImage.hidden = true;
+        let newDot = document.createElement("span");
+        newDot.setAttribute("id", "dot" + i);
+        newDot.dataset.position = i;
+        newDot.classList.add("dot");
+        dots.appendChild(newDot);
         pictureFrame.appendChild(newImage);
     }
     pictureFrame.firstChild.hidden = false;
-}
-
-function doSomething() {
-    console.log("Do something");
+    dots.firstChild.classList.add("dot-chosen");
 }
 
 pictureLoader();
 let pictureNodes = pictureFrame.childNodes;
+let dotNodes = dots.childNodes;
+
 menuButton.addEventListener("click", () => {
     drop(menuOptions);
     if (menuOptions.classList.contains("visible")) {
@@ -41,32 +46,56 @@ menuButton.addEventListener("click", () => {
 
 rightButton.addEventListener("click", ()=> {
     let indexOfLastPicture = 0;
-    //pictureNodes = document.querySelectorAll(".picture")
     pictureNodes.forEach(function (picture, pictureIndex) {
         if (picture.hidden === false) {
             picture.hidden = true;
             indexOfLastPicture = pictureIndex;
+            dotNodes[pictureIndex].classList.remove("dot-chosen");
         }
     })
     if (indexOfLastPicture === pictureNodes.length - 1) {
         pictureNodes[0].hidden = false;
+        dotNodes[0].classList.add("dot-chosen");
     } else {
         pictureNodes[indexOfLastPicture + 1].hidden = false;
+        dotNodes[indexOfLastPicture + 1].classList.add("dot-chosen");
     }
 })
-
+let indexOfLastPicture = 0;
 leftButton.addEventListener("click", ()=> {
-    let indexOfLastPicture = 0;
-    //pictureNodes = document.querySelectorAll(".picture")
     pictureNodes.forEach(function (picture, pictureIndex) {
         if (picture.hidden === false) {
             picture.hidden = true;
             indexOfLastPicture = pictureIndex;
+            dotNodes[pictureIndex].classList.remove("dot-chosen");
         }
     })
     if (indexOfLastPicture === 0) {
         pictureNodes[pictureNodes.length - 1].hidden = false;
+        dotNodes[pictureNodes.length - 1].classList.add("dot-chosen");
     } else {
         pictureNodes[indexOfLastPicture - 1].hidden = false;
+        dotNodes[indexOfLastPicture - 1].classList.add("dot-chosen");
     }
+})
+
+/* dots.addEventListener("click", (e)=> {
+    if (condition) {
+        console.log(e.target.getAttribute("id"));
+    }
+}) */
+
+dotNodes.forEach((dot) => {
+    dot.addEventListener("click", ()=> {
+        pictureNodes.forEach(function (picture, pictureIndex) {
+            if (picture.hidden === false) {
+                picture.hidden = true;
+                indexOfLastPicture = pictureIndex;
+                dotNodes[pictureIndex].classList.remove("dot-chosen");
+            }
+        })
+        pictureNodes[dot.dataset.position].hidden = false;
+        dotNodes[dot.dataset.position].classList.add("dot-chosen");
+        console.log(pictureNodes[dot.dataset.position]);
+    })
 })
